88427b68540d95988f4f6fa296a709b0c70b0a7f
Валерий Кулаков <kulakov.angel@gmail.com>
2021-03-06 19:51:41 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messages, setMessages] = useState([]);
+    const [ws] = useState(new WebSocket('wss://arcane-forest-49375.herokuapp.com/'));
+    const [formData, setFormData] = useState({name: '', message: ''});
+
+
+    // useEffect(() => {
+    //     setWs(new WebSocket('wss://arcane-forest-49375.herokuapp.com/'));
+    // });
+
+    useEffect(() => {
+        ws.addEventListener('message', (evt) => {
+            const data = JSON.parse(evt.data);
+            setMessages((prevState) => ([...prevState, data]));
+        });
+    }, []);
+
+    const handleChangeFormData = (evt) => {
+        setFormData({
+            ...formData,
+            [evt.target.name]: evt.target.value,
+        })
+    };
+
+    const handleSendMessage = (evt) => {
+        evt.preventDefault();
+        ws.send(JSON.stringify({
+            name: formData.name,
+            message: formData.message,
+        }));
+        setFormData({
+            ...formData,
+            message: '',
+        })
+    };
+
+    return (
+        <main>
+            <h1>Chat App</h1>
+            <form onSubmit={handleSendMessage}>
+                <input name={'name'} onChange={handleChangeFormData} value={formData.name}/>
+                <textarea name={'message'} onChange={handleChangeFormData} value={formData.message}/>
+                <input type={'submit'}/>
+                <h2>Messages:</h2>
+                <ul>
+                    {
+                        messages.map(message => (
+                            <li><i>{message.name}</i>: {message.message}</li>
+                        ))
+                    }
+                </ul>
+            </form>
+        </main>
+    );
 }
 
 export default App;
